2d31ac87edf66f181a0fcbc792c920ea19dfd94e
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-01-29 11:53:02 -0300

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,5 @@
+f=int(input())
+l=["I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i,j in enumerate(l):
+    if(i+1==f):
+        print(j)
