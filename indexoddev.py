a1d30fe72adcad65157b7837c3f32afdbce9024f
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-02-03 08:17:10 -0300

    Update and rename indexoddeve.py to indexoddev.py

--- indexoddev.py
+++ indexoddev.py
@@ -2,7 +2,7 @@ u=int(input())
 o=list(map(int,input().split()))
 li =[]
 for i,j in enumerate(o):
-  if (((i%2==0) and (j%2 !=0)) or ((i%2 !=0) and (j%2 ==0))):
+  if (((i%2!=0) and (j%2 ==0)) or ((i%2 ==0) and (j%2 !=0))):
     p=j
     li.append(p)
 print(*li)
