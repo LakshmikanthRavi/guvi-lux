5586d3bba39d745a9f9320dd31a5d7f182dcb461
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-02-03 07:46:37 -0300

    Update and rename holiday.py to holidy.py

--- holiday.py
+++
@@ -1,6 +0,0 @@
-u=input()
-o=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-if u==o[0]:
-  print("yes")
-else:
-  print("no")
---
+++ holidy.py
@@ -0,0 +1,12 @@
+u=input()
+o=["Sunday","Saturday"]
+l=["Monday","Tuesday","Wednesday","Thursady","Friday"]
+
+if u in o:
+  print("yes")
+  
+elif u in l:
+   print("no")
+
+
+
