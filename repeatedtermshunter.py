2a38dd8bea90fc9b50389983c36f57cc390035a7
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-02-12 16:05:37 +0530

    Update repeatedtermshunter.py

--- repeatedtermshunter.py
+++ repeatedtermshunter.py
@@ -1,9 +1,12 @@
 p=int(input())
 o=list(map(int,input().split()))
+o.sort()
 li=[]
-for i in range(p-1):
+for i in range(0,p-1):
   if o[i]==o[i+1]:
-    li.append(o[i])
+    if o[i] not in li:
+      li.append(o[i])
+li.sort()
 if len(li)!=0:
   print(*li)
 elif len(li)==0:
