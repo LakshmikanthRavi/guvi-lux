872f088176798497b4c01dca37e9d5dd63cff416
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-02-14 15:51:34 +0530

    Create pro57.py

---
+++ pro57.py
@@ -0,0 +1,15 @@
+y,k=map(int,input().split())
+j,h=map(int,input().split())
+v,b=map(int,input().split())
+g,m=map(int,input().split())
+u=y+j+v+g
+l=k+h+b+m
+if u==l:
+  print("yes")
+else:
+  print("no")
+
+
+
+
+  
