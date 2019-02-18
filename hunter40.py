913cf6322a6d793c048b5f14bd0840283f746fd2
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-02-18 12:10:27 -0300

    Update hunter40.py

--- hunter40.py
+++ hunter40.py
@@ -8,17 +8,17 @@ for i in range(len(str(y))):
   y=y//10
 g=t
 if(len(str(g))==1):
-  print("yes")
+  print("YES")
 else:
   for i in range(len(str(g))):
     f=g%10
     l=l*10+f
     g=g//10
     if l==g:
-      print("yes")
+      print("YES")
       break
   else:
-    print("no")
+    print("NO")
 
   
 
