9e90f1d5a5924e6ce2bfd99167961fd8e7949f7a
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-02-18 12:08:42 -0300

    Create hunter40.py

---
+++ hunter40.py
@@ -0,0 +1,26 @@
+y=int(input())
+li=[]
+t=0
+l=0
+for i in range(len(str(y))):
+  c=y%10
+  t=t+c
+  y=y//10
+g=t
+if(len(str(g))==1):
+  print("yes")
+else:
+  for i in range(len(str(g))):
+    f=g%10
+    l=l*10+f
+    g=g//10
+    if l==g:
+      print("yes")
+      break
+  else:
+    print("no")
+
+  
+
+  
+
