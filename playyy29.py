aba6c2aa66a84ce7b94dc48f443b7ddf74a81c45
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-03-17 15:33:08 -0200

    Update playyy29.py

--- playyy29.py
+++ playyy29.py
@@ -1,8 +1,12 @@
 u,k=map(int,input().split())
-c=1
-for i in range(u,k+1):
+c=0
+li=[]
+for i in range(1,k+1):
   o=i
   g=o**2
-  if g==i:
+  li.append(g)
+for i in range(u,k+1):
+  if i in li:
     c=c+1
 print(c)
+
