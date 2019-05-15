2084875369481bae70b077db1c380714fe2b7e2d
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-05-15 08:57:46 +0530

    Update playeer59.py

--- playeer59.py
+++ playeer59.py
@@ -1,13 +1,12 @@
 s=int(input())
-h1=int(input())
-s1=list(str(h1))
+s1=list(map(int,input().split()))
 li=[]
 l=[]
 for i in range(0,s-1):
     if s1[i]>=s1[i+1]:
         li.append(s1[i])
 for i in li:
-    if int(i)>0:
+    if i>0:
         l.append(i)
 print(*l)
 
