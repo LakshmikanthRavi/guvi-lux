c8fe28e529d03491fa764428dd73a541e80405c1
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-05-21 12:23:34 +0530

    Create proo21.py

---
+++ proo21.py
@@ -0,0 +1,32 @@
+v=int(input())
+u=list(map(int,input().split()))
+li=[]
+p=[]
+if v%2==0:
+    c=1
+    f=v//2
+else:
+    c=0
+    g=(v//2)+1
+if c==1:
+    li.append(u[f:])
+    li.append(u[0:f])
+    for i in li:
+        p.append(sum(i))
+    f=p[0]//len(p)
+    o=p[1]//len(p)
+    if f==o:
+        print("yes")
+    else:
+        print("no")
+if c==0:
+    li.append(u[g:])
+    li.append(u[:g])
+    for i in li:
+        p.append(sum(i))
+    f=p[0]//len(p)
+    o=p[1]//len(p)
+    if f==o:
+        print("yes")
+    else:
+        print("no")
