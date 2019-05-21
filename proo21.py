880ee684360592545a6849c24725b45b11ec9925
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-05-21 12:40:28 +0530

    Update proo21.py

--- proo21.py
+++ proo21.py
@@ -20,12 +20,16 @@ if c==1:
     else:
         print("no")
 if c==0:
-    li.append(u[g:])
     li.append(u[:g])
     for i in li:
+        x=len(i)
+    li.append(u[g:])
+    for i in li:
+        b=len(i)
+    for i in li:
         p.append(sum(i))
-    f=p[0]//len(p)
-    o=p[1]//len(p)
+    f=p[0]//x
+    o=p[1]//b
     if f==o:
         print("yes")
     else:
