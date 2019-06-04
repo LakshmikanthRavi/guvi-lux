c048c6a45ff5e7d773226f56b26059dc67005ec8
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-06-04 19:54:04 +0530

    Update hunter28.py

--- hunter28.py
+++ hunter28.py
@@ -1,11 +1,7 @@
 n=input()
-l=list(n)
-l.sort()
-li=[]
-for i in range(0,len(n)-1):
-    if(l[i]==l[i+1]):
-        if l[i] not in li:
-            li.append(l[i])
-li.sort()
-x="".join(li)
+l=[]
+for i in n:
+    if i not in l:
+        l.append(i)
+x="".join(l)
 print(x)
