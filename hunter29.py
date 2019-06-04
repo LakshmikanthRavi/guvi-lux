92025fd23ca2261ec5c74c8f344a5ae9941c8692
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-06-04 20:20:04 +0530

    Update hunter29.py

--- hunter29.py
+++ hunter29.py
@@ -1,11 +1,13 @@
 g=int(input())
 n=list(map(int,input().split()))
 li=[]
-for i in range(0,len(n)+1):
-    li.append(sum(n[0:i]))
+for i in range(0,g):
+    for j in range(i,g+1):
+        li.append(sum(n[i:j]))
 g=max(li)
 if g==0:
     print(li[1])   
 else:
     print(g)
     
+
