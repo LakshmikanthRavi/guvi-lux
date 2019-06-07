3c4350d2e15fc666fbe4446afd8ffa37506ca1ba
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-06-07 22:10:53 +0530

    Create player18.py

---
+++ player18.py
@@ -0,0 +1,12 @@
+z=int(input())
+li=[]
+c=0
+n=0
+for i in range(0,z):
+    h=input()
+    g=list(h)
+    if "k" in g and "a" in g and "b" in g and "a" in g and "l" in g and "i" in g:
+        li.append(n)
+for i in li:
+    c=c+1
+print(c)
