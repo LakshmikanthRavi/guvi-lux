3ee7ce71053e86b01d9135e6b4c8fd802bdab1e5
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-06-07 21:21:01 +0530

    Create proo40.py

---
+++ proo40.py
@@ -0,0 +1,12 @@
+f=input()
+d="dhoni"
+y=len(d)
+o=len(f)
+if y==o:
+    if "d" in f and "h" in f and "o" in f and "n" in f and "i" in f:
+        print("yes")
+    else:
+        print("no")
+else:
+    print("no")
+    
