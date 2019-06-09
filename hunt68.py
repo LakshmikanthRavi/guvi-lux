4dd05bcb7379c419a005a8a0435170ae0c0309fb
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-06-09 22:35:41 +0530

    Create hunt68.py

---
+++ hunt68.py
@@ -0,0 +1,11 @@
+h=int(input())
+g=list(map(int,input().split()))
+l=[]
+o=min(g)
+k=max(g)
+for i,j in enumerate(g):
+    if j==o:
+        l.append(i+1)
+    if j==k:
+        l.append(i+1)
+print(*l)
