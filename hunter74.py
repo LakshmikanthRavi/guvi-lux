d4d675ceec2073e8d9b39cc6a4e1de3bd9200342
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-06-11 18:16:50 +0530

    Create hunter74.py

---
+++ hunter74.py
@@ -0,0 +1,28 @@
+f=input()
+g=input()
+l=[]
+li=[]
+p=[]
+o=[]
+z=[]
+q=[]
+for i in range(0,len(f)):
+    for j in range(0,len(f)+1):
+        l.append(f[i:j])
+for i in range(0,len(g)):
+    for j in range(0,len(g)+1):
+        li.append(g[i:j])
+for i in l:
+    for j in li:
+        if i==j:
+            p.append(i)
+for i in p:
+    if i != '':
+        o.append(i)
+for i in o:
+    z.append(len(i))
+v=max(z)
+for i in o:
+    if len(i)==v:
+        q.append(i)
+print(max(q))
