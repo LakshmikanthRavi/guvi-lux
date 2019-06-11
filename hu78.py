d10d2d5bbb1a06179a9dd3d52aa5a8b7866185fe
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-06-11 18:36:06 +0530

    Update and rename hunt76.py to hu78.py

---
+++ hu78.py
@@ -0,0 +1,4 @@
+s,d=map(int,input().split())
+for i in range(0,d):
+    u,v=map(int,input().split())
+print(d)    
--- hunt76.py
+++
@@ -1,4 +0,0 @@
-h,k=map(int,input().split())
-for i in range(0,k):
-    u,v=map(int,input().split())
-print(k)    
