f85f97a6774a90a27108c361e418cbcc18a84371
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-07-05 22:05:54 +0530

    Update and rename pro111111.py to pro111.py

---
+++ pro111.py
@@ -0,0 +1,5 @@
+o=int(input())
+if o%2=0: 
+    print(o*(o//2)-(o//2))
+else:
+    print(o*(o//2))
--- pro111111.py
+++
@@ -1,5 +0,0 @@
-u=int(input())
-if u%2=0: 
-    print(u*(u//2)-(u//2))
-else:
-    print(u*(u//2))
