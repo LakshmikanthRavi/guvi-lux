190ff423160fc106992c3fa6a3ecab04145417ac
LakshmikanthRavi <46441168+LakshmikanthRavi@users.noreply.github.com>
2019-07-19 12:56:37 +0530

    Update hunter10.py

--- hunter10.py
+++ hunter10.py
@@ -1,10 +1,13 @@
 f,e=map(int,input().split())
 k=list(map(int,input().split()))
 g=list(map(int,input().split()))
-for i in k:
-    if i in g:
-        print("YES")
-        break
+if len(g)<len(k):
+    for i in g:
+        if i in k:
+            print("YES")
+            break
+    else:
+        print("NO")
 else:
     print("NO")
     
